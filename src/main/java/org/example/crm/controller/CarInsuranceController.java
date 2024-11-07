d56437f0a3d954137d343e8b400b7b36fbd78784
HUTII <hutii2004@gmail.com>
2024-11-07 15:43:44 +0800

    initialize

---
+++ src/main/java/org/example/crm/controller/CarInsuranceController.java
@@ -0,0 +1,13 @@
+package org.example.crm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hutianlin
+ * 2024/11/07 15:33
+ */
+@RestController
+@RequestMapping("/carInsurance")
+public class CarInsuranceController {
+}
---
+++ src/main/java/org/example/crm/controller/TravelInsuranceController.java
@@ -0,0 +1,13 @@
+package org.example.crm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hutianlin
+ * 2024/11/07 15:34
+ */
+@RestController
+@RequestMapping("/travelInsurance")
+public class TravelInsuranceController {
+}
