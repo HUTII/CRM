cd91718d2044133616a6e72a10a73a91ab3de615
HUTII <hutii2004@gmail.com>
2024-11-28 01:24:41 +0800

    添加注释

--- src/main/java/org/example/crm/config/CorsConfig.java
+++ src/main/java/org/example/crm/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
+ * 跨域配置
  * @author hutianlin
  * 2024/11/26 20:20
  */
